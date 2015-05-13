69784632ca123e99a5154d3e450d8173dd2ddf41
Sigmund Burgos <sigmund.utech@gmail.com>
2015-05-13 15:34:32 +0800

    changed talent_social primary key to talentSocialId

--- src/resources/talent_social.js
+++ src/resources/talent_social.js
@@ -3,6 +3,6 @@
 var api = require('src/config/api.js'),
 	Resource = require('src/services/resource.js');
 
-module.exports = new Resource(api.base + '/talent/talent_social/:talentId', {
+module.exports = new Resource(api.base + '/talent/talent_social/:talentSocialId', {
 	model: 'bam_talent_social'
 });
