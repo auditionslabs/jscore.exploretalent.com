6e5cdfe34235815d0de0cf18e91ed4de56dd360b
Sigmund Burgos <sigmund.utech@gmail.com>
2015-05-14 12:01:32 +0800

    added conversion for lb and kg

--- src/services/converter.js
+++ src/services/converter.js
@@ -11,6 +11,14 @@ Converter.prototype.cmToInch = function(cm) {
 	return cm * 0.393701;
 };
 
+Converter.prototype.lbToKg = function(lb) {
+	return lb * 0.453592;
+};
+
+Converter.prototype.kgToLb = function(kg) {
+	return kg * 2.20462;
+};
+
 Converter.prototype.convert = function(value, from, to, round) {
 	from = from.toLowerCase();
 	to = to.charAt(0).toUpperCase() + to.substr(1).toLowerCase();
