7cd32446aae506594e70c0865d3a2343dd1b4b49
Josh Dabuet <joshd.utech@gmail.com>
2015-06-16 17:00:58 +0800

    fixes in favorite_project resource

--- src/resources/favorite_projects.js
+++ src/resources/favorite_projects.js
@@ -3,6 +3,6 @@
 var api = require('src/config/api.js'),
 	Resource = require('src/services/resource.js');
 
-module.exports = new Resource(api.base + '/talent/favorite_projects/:project_id', {
+module.exports = new Resource(api.base + '/talent/favorite_projects/:projectId', {
 	model: 'bam_favorite_castings'
 });
