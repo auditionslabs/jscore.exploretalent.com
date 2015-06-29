65af74383cd2c4250517238435c51d517684a86c
Sigmund Burgos <sigmund.burgos@gmail.com>
2015-06-29 18:33:30 +0800

    Added to and from for message.

--- src/models/message.js
+++ src/models/message.js
@@ -6,6 +6,14 @@ function Message(data) {
 	_.extend(this, data);
 }
 
+Message.prototype.from = function() {
+	return _.first(_.where(this.users, { id : this.user_id }));
+};
+
+Message.prototype.to = function() {
+	return _.first(_.filter(this.users, function(n) { return n.id != this.user_id }));
+}
+
 Message.relationship = [
 ];
 
