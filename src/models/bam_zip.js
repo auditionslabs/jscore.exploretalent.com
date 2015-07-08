b449fae99ebdae0f439239313b91ef7835ceabe0
Sigmund Burgos <sigmund.burgos@gmail.com>
2015-07-08 16:20:16 +0800

    Added zip model and resource.

---
+++ src/models/bam_zip.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var _ = require('lodash');
+
+function Zip(data) {
+	_.extend(this, data);
+}
+
+module.exports = Zip;
---
+++ src/resources/zip.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var api = require('src/config/api.js'),
+	Resource = require('src/services/resource.js');
+
+module.exports = new Resource(api.base + '/zips/:zip', {
+	model: 'bam_zip'
+});
