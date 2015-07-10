782a73c3bc5a85d418a82c9bc8ddd64d526d7a43
Daniel Cabale <dc.utech@gmail.com>
2015-07-10 21:36:35 +0800

    change job order

--- src/resources/job_orders.js
+++ src/resources/job_orders.js
@@ -4,7 +4,7 @@ var api = require('src/config/api.js'),
 	Resource = require('src/services/resource.js');
 
 module.exports = new Resource(api.base + '/talent/job_orders/:jobId', {
-	model: 'bam_job_order'
+	model: 'job_order'
 });
 
 //
