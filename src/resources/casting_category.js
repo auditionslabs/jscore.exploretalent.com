254bae3fd235e79354d521790aae64223bd80f10
Sigmund Burgos <sigmund.burgos@gmail.com>
2015-08-05 21:54:53 +0800

    Changed casting_category identifier to castingCategoryId.

--- src/resources/casting_category.js
+++ src/resources/casting_category.js
@@ -3,6 +3,6 @@
 var api = require('src/config/api.js'),
 	Resource = require('src/services/resource.js');
 
-module.exports = new Resource(api.base + '/talent/casting_categories/:talentId', {
+module.exports = new Resource(api.base + '/talent/casting_categories/:castingCategoryId', {
 	model: 'bam_cast_cat'
 });
