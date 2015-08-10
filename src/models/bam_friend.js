1da4a0d2294eb7d81c39ff69babc8495e3b4eb7f
Sigmund Burgos <sigmund.burgos@gmail.com>
2015-08-10 18:41:24 +0800

    Added getOtherUser for friend model.

--- src/models/bam_friend.js
+++ src/models/bam_friend.js
@@ -3,14 +3,30 @@
 var _ = require('lodash'),
 	converter = require('../services/converter.js');
 
-function Friends(data) {
+function Friend(data) {
 	_.extend(this, data);
 }
 
-Friends.relationship = [
+Friend.prototyp.getOtherUser = function() {
+	if (this.owner) {
+		var owner = this.owner;
+
+		if (this.bam_talentnum1 == owner.bam_talentci.talentnum) {
+			return this.bam_talentci1;
+		}
+		else {
+			return this.bamt_talentci2;
+		}
+	}
+	else {
+		return null;
+	}
+}
+
+Friend.relationship = [
 	'bam_talentci1:bam_talentci',
 	'bam_talentci2:bam_talentci',
 	'data:bam_friends'
 ];
 
-module.exports = Friends;
+module.exports = Friend;
