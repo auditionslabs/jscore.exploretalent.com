1e733ce44f4a5f052c3d9d3c39a6a0146821e3f6
Daniel Cabale <dc.utech@gmail.com>
2015-08-12 23:58:55 +0800

    schedules-v1

--- src/models/schedule.js
+++ src/models/schedule.js
@@ -9,7 +9,9 @@ function Schedule(data) {
 Schedule.relationship = [
 	'invitee:user',
 	'inviter:user',
-	'bam_role'
+	'bam_role',
+	//if using GET from api, result is in the data property, set model to array of bam_talentcis
+	'data:schedules'
 ];
 
 module.exports = Schedule;
