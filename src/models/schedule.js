de19ae05d307827ba5f015eb97a7a50ab5975eba
Daniel Cabale <dc.utech@gmail.com>
2015-08-13 18:35:45 +0800

    schedule v1

--- src/models/schedule.js
+++ src/models/schedule.js
@@ -1,10 +1,15 @@
 'use strict';
 
-var _ = require('lodash');
+var _ = require('lodash'),
+	date = require('../services/date.js');
 
 function Schedule(data) {
 	_.extend(this, data);
 }
+
+Schedule.prototype.convertToFullDate = function(timestamp) {
+	return date.formatYMD(parseInt(timestamp));
+};
 
 Schedule.relationship = [
 	'invitee:user',
