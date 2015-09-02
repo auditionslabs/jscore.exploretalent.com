94a591437c3f64202da48b01fced4df0d7e2e309
Sigmund Burgos <sigmund.burgos@gmail.com>
2015-09-02 22:18:55 +0800

    Added type in hasQuery.

--- src/models/campaign_query.js
+++ src/models/campaign_query.js
@@ -6,11 +6,16 @@ function CampaignQuery(data) {
 	_.extend(this, data || {});
 }
 
-CampaignQuery.prototype.hasQuery = function(key) {
+CampaignQuery.prototype.hasQuery = function(key, type) {
 	var query = JSON.parse(this.query);
 
 	return _.find(query.wheres, function(q) {
-		return q[2] == key;
+		if (type == 'min')
+			return q[1] == key && q[2] == '>';
+		else if (type == 'max')
+			return q[1] == key && q[2] == '<';
+		else
+			return q[1] == key;
 	});
 }
 
