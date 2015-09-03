b316a33b5337762a5979464217c6363e090e61d9
Sigmund Burgos <sigmund.burgos@gmail.com>
2015-09-03 17:32:34 +0800

    Updated hasQuery.

--- src/models/campaign_query.js
+++ src/models/campaign_query.js
@@ -10,16 +10,16 @@ CampaignQuery.prototype.hasQuery = function(key, type) {
 	var query = JSON.parse(this.query);
 
 	return _.find(query.wheres, function(q) {
-		if (q[2] instanceof Array) {
-			return q[2][1] == key;
-		}
-		else {
+		if (q[0] == 'where') {
 			if (type == 'min')
 				return q[1] == key && q[2] == '>=';
 			else if (type == 'max')
 				return q[1] == key && q[2] == '<=';
 			else
 				return q[1] == key;
+		}
+		else if (q[0] == 'whereHas') {
+			return q[2][1] == key;
 		}
 	});
 }
