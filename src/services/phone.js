2162f6ef3104cddf1fbf24b91a6f01e46218d8c6
Sigmund Burgos <sigmund.burgos@gmail.com>
2015-09-07 18:00:49 +0800

    Added checking to prevent errors.

--- src/services/phone.js
+++ src/services/phone.js
@@ -5,7 +5,8 @@ function unmask(phone) {
 }
 
 function mask(phone) {
-	return "(" + phone.substring(0, 3) + ")" + phone.substring(3,6) + "-" + phone.substring(6,10);
+	if (phone.length == 10)
+		return "(" + phone.substring(0, 3) + ")" + phone.substring(3,6) + "-" + phone.substring(6,10);
 }
 
 module.exports = {
