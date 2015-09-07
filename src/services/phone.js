2791b5ccaa1b49c94c264d3142fc657dcc5f6493
Sigmund Burgos <sigmund.burgos@gmail.com>
2015-09-07 17:58:52 +0800

    Added mask phone.

--- src/services/phone.js
+++ src/services/phone.js
@@ -4,6 +4,11 @@ function unmask(phone) {
 	return phone.replace(/\-/g, '').replace(/\(/g, '').replace(/\)/g, '');
 }
 
+function mask(phone) {
+	return "(" + phone.substring(0, 3) + ")" + phone.substring(3,6) + "-" + phone.substring(6,10);
+}
+
 module.exports = {
-	unmask : unmask
+	unmask : unmask,
+	mask : mask
 };
