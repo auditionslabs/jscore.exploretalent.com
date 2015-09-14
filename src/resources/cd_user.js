fe6777d23f4170d443d28c362a46a9185ae396fa
Sigmund Burgos <sigmund.burgos@gmail.com>
2015-09-14 20:14:57 +0800

    Change cd user route.

--- src/resources/cd_user.js
+++ src/resources/cd_user.js
@@ -3,6 +3,6 @@
 var api = require('src/config/api.js'),
 	Resource = require('src/services/resource.js');
 
-module.exports = new Resource(api.base + api.type + '/cd_user/:cdUserId', {
+module.exports = new Resource(api.base + api.type + '/cd_users/:cdUserId', {
 	model: 'bam_cd_user'
 });
