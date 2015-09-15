89a8655afaeece4cf5a8c41529d6b02ec44c62c8
Sigmund Burgos <sigmund.burgos@gmail.com>
2015-09-15 19:16:49 +0800

    Removed role model functions.

--- src/models/bam_role.js
+++ src/models/bam_role.js
@@ -7,22 +7,6 @@ function Role(data) {
 	_.extend(this, data);
 }
 
-Role.prototype.convertToFullDate = function(timestamp) {
-	return date.formatYMD(parseInt(timestamp));
-};
-
-Role.prototype.getHeightMinText = function() {
-	var feet = Math.floor(this.height_min / 12.00);
-	var inches = feet % 12;
-	return feet + '"' + inches + "'";
-}
-
-Role.prototype.getHeightMaxText = function() {
-	var feet = Math.floor(this.height_max / 12.00);
-	var inches = feet % 12;
-	return feet + '"' + inches + "'";
-}
-
 Role.relationship = [
 	'data:bam_roles'
 ];
