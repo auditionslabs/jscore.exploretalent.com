ae7436a2cfdd2867e70d4bd8ae78a69d9eb2ee7f
Daniel Cabale <dc.utech@gmail.com>
2015-09-15 15:04:39 +0800

    projects-v2

--- src/models/project.js
+++ src/models/project.js
@@ -7,8 +7,33 @@ function Project(data) {
 	_.extend(this, data);
 }
 
-Schedule.prototype.convertToFullDate = function(timestamp) {
+Project.prototype.convertToFullDate = function(timestamp) {
 	return date.formatYMD(parseInt(timestamp));
 };
+
+Project.prototype.heightText = function() {
+	var feet = Math.floor(this.bam_talentinfo1.heightinches / 12.00);
+	var inches = feet * 12;
+	return feet + '"' + inches + "'";
+}
+
+Project.prototype.getLocation = function() {
+	if (this.bam_talentinfo2 && this.bam_talentinfo2.city1) {
+		return this.bam_talentinfo2.city1;
+	}
+	else if(this.city && this.state && this.state.length > 2) {
+		return this.city + ', ' + this.state;
+	}
+	else if(this.state && this.state.length > 2) {
+		return this.city;
+	}
+	else {
+		return 'Not Set';
+	}
+}
+
+Project.prototype.getAge = function() {
+	return date.calculateAge(this.bam_talentinfo1.dobyyyy, this.bam_talentinfo1.dobmm, this.bam_talentinfo1.dobdd);
+}
 
 module.exports = Project;
