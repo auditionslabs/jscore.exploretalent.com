faf41d65aa6c144134fa89552b01476836472cbe
Daniel Cabale <dc.utech@gmail.com>
2015-09-15 15:26:50 +0800

    projects-v2

--- src/models/bam_role.js
+++ src/models/bam_role.js
@@ -11,8 +11,14 @@ Role.prototype.convertToFullDate = function(timestamp) {
 	return date.formatYMD(parseInt(timestamp));
 };
 
-Role.prototype.heightText = function() {
-	var feet = Math.floor(this.heightinches / 12.00);
+Role.prototype.heightMinText = function() {
+	var feet = Math.floor(this.height_min / 12.00);
+	var inches = feet * 12;
+	return feet + '"' + inches + "'";
+}
+
+Role.prototype.heightMaxText = function() {
+	var feet = Math.floor(this.height_max / 12.00);
 	var inches = feet * 12;
 	return feet + '"' + inches + "'";
 }
