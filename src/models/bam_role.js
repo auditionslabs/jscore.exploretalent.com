aaff94efaa0b97f6eebc52b0a81deb9706a7bd37
Sigmund Burgos <sigmund.burgos@gmail.com>
2015-09-15 19:24:04 +0800

    added height texts in role model.

--- src/models/bam_role.js
+++ src/models/bam_role.js
@@ -7,6 +7,18 @@ function Role(data) {
 	_.extend(this, data);
 }
 
+Role.prototype.getHeightMinText = function() {
+	var feet = Math.floor(this.height_min / 12.00);
+	var inches = feet % 12;
+	return feet + '"' + inches + "'";
+}
+
+Role.prototype.getHeightMaxText = function() {
+	var feet = Math.floor(this.height_max / 12.00);
+	var inches = feet % 12;
+	return feet + '"' + inches + "'";
+}
+
 Role.relationship = [
 	'data:bam_roles'
 ];
