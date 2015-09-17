c11f5287968251aa07382e50f4852a7ef380e1b3
Sigmund Burgos <sigmund.burgos@gmail.com>
2015-09-17 16:26:58 +0800

    Added like it list for role.

--- src/resources/job.js
+++ src/resources/job.js
@@ -1,5 +1,10 @@
 'use strict';
 
-var project = require('./project.js');
+var project = require('./project.js'),
+	Resource = require('src/services/resource.js');
+
+Resource.prototype.getLikeItList = function() {
+	console.log('test');
+}
 
 module.exports = project.child('/jobs/:jobId');
