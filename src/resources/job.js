f2bab078bb58a7ac88ae742201888cd8d778fb8f
Sigmund Burgos <sigmund.burgos@gmail.com>
2015-09-17 16:11:15 +0800

    Fixed url for roles.

--- src/resources/job.js
+++ src/resources/job.js
@@ -3,6 +3,6 @@
 var api = require('src/config/api.js'),
 	Resource = require('src/services/resource.js');
 
-module.exports = new Resource(api.base + api.type + '/jobs/:jobId', {
+module.exports = new Resource(api.base + api.type + '/projects/:projectId/jobs/:jobId', {
 	model: 'bam_role'
 });
