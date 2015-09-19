e3f173fb508b09950e532be83b074b237e3f7922
Sigmund Burgos <sigmund.burgos@gmail.com>
2015-09-19 19:42:02 +0800

    Added role functions.

--- src/models/bam_role.js
+++ src/models/bam_role.js
@@ -19,10 +19,154 @@ Role.prototype.getHeightMaxText = function() {
 	return feet + '"' + inches + "'";
 }
 
-Role.prototype.getLikeItList = function() {
-	return _.filter(this.schedules, function(schedule) {
-		return schedule.rating > 0;
-	});
+Role.prototype.getGenders() = function() {
+	var array = [];
+
+	if (this.gender_male == 1)
+		array.push('Male');
+
+	if (this.gender_female == 1)
+		array.push('Female');
+
+	return array;
+}
+
+Role.prototype.getEthnicities = function() {
+	var array = [];
+
+	if (this.ethnicity_any == 1)
+		return array;
+
+	var ethnicities = {
+		african		: 'African',
+		african_am	: 'African American',
+		asian		: 'Asian',
+		carribian	: 'Caribbean',
+		caucasian	: 'Caucasian',
+		hispanic	: 'Hispanic',
+		mediterranean	: 'Mediterranean',
+		middle_est	: 'Middle Eastern',
+		mixed		: 'Mixed',
+		native_am	: 'American',
+		american_in	: 'American Indian',
+		east_indian	: 'East Indian'
+	}
+
+	var (var e in ethnicities) {
+		if (this['ethnicity_' + e] == 1) {
+			array.push(ethnicities[e]);
+		}
+	}
+
+	return array;
+}
+
+Role.prototype.getHairColors = function() {
+	var array = [];
+
+	if (this.hair_any == 1)
+		return array;
+
+	var haircolors = {
+		auburn		: 'Auburn',
+		black		: 'Black',
+		blonde		: 'Blonde',
+		brown		: 'Brown',
+		chestnut	: 'Chestnut',
+		dark_brown 	: 'Dark Brown',
+		grey		: 'Grey',
+		red			: 'Red',
+		white		: 'White',
+		salt_pepper : 'Salt&Peppe'
+	};
+
+	var (var color in haircolors) {
+		if (this['hair_' + color] == 1) {
+			array.push(haircolors[color]);
+		}
+	}
+
+	return array;
+}
+
+Role.prototype.getHairStyles = function() {
+	var array = [];
+
+	if (this.hairstyle_any == 1)
+		return array;
+
+	var hairstyles = {
+		afro 	: 'Afro',
+		bald	: 'Bald',
+		buzz	: '',
+		cons	: 'Conservati',
+		dread	: 'Dreadlocks',
+		long	: 'Long',
+		medium	: 'Medium',
+		shaved	: 'Shaved',
+		short	: 'Short'
+	}
+
+	var (var style in hairstyles) {
+		if (this['hairstyle_' + style] == 1) {
+			array.push(hairstyles[style]);
+		}
+	}
+
+	return array;
+}
+
+Role.prototype.getEyeColors = function() {
+	var array = [];
+
+	if (this.eye_any == 1)
+		return array;
+
+	var eyecolors = {
+		blue	: 'Blue',
+		b_g		: 'Blue-Green',
+		brown	: 'Brown',
+		green	: 'Green',
+		grey	: 'Grey',
+		g_b		: 'Grey-Blue',
+		g_g		: 'Grey-Green',
+		hazel	: 'Hazel'
+	}
+
+	var (var color in eyecolors) {
+		if (this['eye_' + color] == 1) {
+			array.push(eyecolors[color]);
+		}
+	}
+
+	return array;
+}
+
+Role.prototype.getBuilds = function() {
+	var array = [];
+
+	if (this.built_any == 1)
+		return array;
+
+	var builds = {
+		medium		: 'Medium',
+		athletic	: 'Athletic',
+		bb			: 'Body Builder',
+		xlarge		: 'Extra Large',
+		large		: 'Large',
+		petite		: 'Petite',
+		thin		: 'Thin',
+		lm			: 'Lean Muscle',
+		average		: 'Average'
+	}
+
+	var (var b in builds) {
+		if (this['built_' + b] == 1) {
+			array.push(builds[b]);
+		}
+	}
+
+	return array;
 }
 
 Role.relationship = [
