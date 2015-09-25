be978ac8b2feb6c58669b8b43cc41c5c36455745
Sigmund Burgos <sigmund.burgos@gmail.com>
2015-09-25 15:33:37 +0800

    Change query_string to function.

--- src/services/query_string.js
+++ src/services/query_string.js
@@ -1,7 +1,5 @@
 'use strict';
 
-function toObject(str) {
+module.exports = function(str) {
 	return (str || document.location.search).replace(/(^\?)/,'').split("&").map( function(n) { if (n) return n = n.split("="),this[n[0]] = n[1],this; }.bind({}))[0] || {};
 }
-
-module.exports = { toObject : toObject };
