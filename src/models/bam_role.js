9bbcd0d3f01c7dacec291d3498092bd4b1fdc6b4
Sigmund Burgos <sigmund.burgos@gmail.com>
2015-09-28 16:13:46 +0800

    Used role_id instead of casting_id

--- src/models/bam_role.js
+++ src/models/bam_role.js
@@ -20,7 +20,7 @@ Role.prototype.getLikeItList = function(options) {
 	var deferred = $.Deferred();
 
 	var data = {
-		jobId : this.casting_id,
+		jobId : this.role_id,
 		withs : [
 			'invitee.bam_talentci.bam_talentinfo1',
 			'invitee.bam_talentci.bam_talentinfo2',
@@ -55,7 +55,7 @@ Role.prototype.getSelfSubmissions = function(options) {
 	var deferred = $.Deferred();
 
 	var data = {
-		jobId : this.casting_id,
+		jobId : this.role_id,
 		withs : [
 			'inviter.bam_talentci.bam_talentinfo1',
 			'inviter.bam_talentci.bam_talentinfo2',
