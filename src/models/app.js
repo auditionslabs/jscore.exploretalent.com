10c33135252bdcad5a696701785e1efb2beb2680
Sigmund Burgos <sigmund.burgos@gmail.com>
2015-10-02 14:43:57 +0800

    Added app resource and models.

---
+++ src/models/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var _ = require('lodash');
+
+function App(data) {
+	_.extend(this, data || {});
+}
+
+App.relationship = [
+	'data:apps'
+];
+
+module.exports = App;
---
+++ src/models/apps.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var _ = require('lodash'),
+	apps = {},
+	App = require('src/models/app.js');
+
+apps.relationship = App.relationship;
+
+apps.create = function(array) {
+	var modelify = require('src/services/model.js');
+
+	return _.map(array || [], function(item) {
+		return modelify('app', item);
+	});
+};
+
+module.exports = apps;
