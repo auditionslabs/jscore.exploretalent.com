4f0849f3a60ac484c35c93cf1e7ec825a7c69ce6
Sigmund Burgos <sigmund.burgos@gmail.com>
2015-10-07 01:09:06 +0800

    Fixed campaign_app url

--- src/resources/campaign_app.js
+++ src/resources/campaign_app.js
@@ -3,6 +3,6 @@
 var api = require('src/config/api.js'),
 	Resource = require('src/services/resource.js');
 
-module.exports = new Resource(api.base + api.type + '/campaign_app/:campaignAppId', {
+module.exports = new Resource(api.base + api.type + '/campaign_apps/:campaignAppId', {
 	model: 'campaign_app'
 });
