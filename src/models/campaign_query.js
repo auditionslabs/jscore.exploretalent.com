be887d86d50c795f672f6e3128c445182dd93d63
Sigmund Burgos <sigmund.burgos@gmail.com>
2015-10-08 23:33:35 +0800

    Added subquery.

--- src/models/campaign_query.js
+++ src/models/campaign_query.js
@@ -45,6 +45,41 @@ CampaignQuery.prototype.toObject = function(key, operator) {
 		if (q[0] == 'where') {
 			// its a subquery!
 			if (q[1] instanceof Array) {
+				_.each(q[1], function(subq) {
+					var name = subq[1].split('.');
+					name = name[name.length - 1];
+
+					var value = subq[3];
+
+					if (subq[2] == '>=') {
+						value = { gt : subq[3] };
+					}
+					else if (subq[2] == '<=') {
+						value = { lt : subq[3] };
+					}
+
+					// if already assigned, convert to array
+					if (obj[name]) {
+						if (value instanceof Object) {
+							if (value.gt != null) {
+								obj[name]['gt'] = value.gt;
+							}
+							else {
+								obj[name]['lt'] = value.lt;
+							}
+						}
+						else {
+							var arr = [];
+							arr.push(obj[name]);
+							arr.push(value);
+							obj[name] = arr;
+						}
+					}
+					// if not, assign directly
+					else {
+						obj[name] = value;
+					}
+				});
 			}
 			else {
 				// get only last name
