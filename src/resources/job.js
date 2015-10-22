1fe3aeb5f37cf01cc2a70ecbd33643b97e22f6b6
Sigmund Burgos <sigmund.burgos@gmail.com>
2015-10-22 17:54:38 +0800

    Updated job resource url for admin.

--- src/resources/job.js
+++ src/resources/job.js
@@ -1,5 +1,12 @@
 'use strict';
 
-var project = require('src/resources/project.js');
+var api = require('src/config/api.js'),
+	Resource = require('src/services/resource.js'),
+	project = require('src/resources/project.js');
 
-module.exports = project.child('/jobs/:jobId', { model : 'bam_role' });
+if (api.type == '/admin') {
+	module.exports = new Resource(api.base + api.type + '/jobs/:jobId', { model : 'bam_role' });
+}
+else {
+	module.exports = project.child('/jobs/:jobId', { model : 'bam_role' });
+}
