d8358e074ccb900c521fdad3d7b0c1f0774d09f6
Aimee <aimee.utech@gmail.com>
2015-11-03 22:02:45 +0800

    trm script engine

--- src/resources/trm_script.js
+++ src/resources/trm_script.js
@@ -4,5 +4,5 @@ var api = require('src/config/api.js'),
 	Resource = require('src/services/resource.js');
 
 module.exports = new Resource(api.base + api.type + '/trm_scripts/:scriptId', {
-	model: 'trm_scripts'
+	model: 'trm_script'
 });
