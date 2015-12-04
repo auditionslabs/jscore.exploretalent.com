1015c060596dd112a3e2da257ee9bdccd7aeec00
Josh Dabuet <joshd.utech@gmail.com>
2015-12-04 17:04:20 +0800

    TalentOrder resource fix

--- src/resources/talentorder.js
+++ src/resources/talentorder.js
@@ -3,6 +3,6 @@
 var api = require('src/config/api.js'),
 	Resource = require('src/services/resource.js');
 
-module.exports = new Resource(api.base + api.type + '/talentorders/:talentnum', {
+module.exports = new Resource(api.base + '/talentorders/:staffId', {
 	model: 'talentorder'
 });
