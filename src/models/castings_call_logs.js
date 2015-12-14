706578d0cde83f459ad30c02e130ffcabbd22f23
Aimee <aimee.utech@gmail.com>
2015-12-14 15:38:37 +0800

    casting call logs

--- src/models/castings_call_logs.js
+++ src/models/castings_call_logs.js
@@ -1,9 +1,15 @@
 'use strict';
 
-var _ = require('lodash');
+var _ = require('lodash'),
+	callLogs = {};
 
-function CastingCallLogs(data) {
-	_.extend(this, data);
-}
 
-module.exports = CastingCallLogs;
+callLogs.create = function(array) {
+	var modelify = require('src/services/model.js');
+
+	return _.map(array || [], function(item) {
+		return modelify('castings_call_log', item);
+	});
+};
+
+module.exports = callLogs;
