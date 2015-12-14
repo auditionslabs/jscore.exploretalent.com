49e8118e8602730ec4abd2edf067d1ff7229d06c
Aimee <aimee.utech@gmail.com>
2015-12-14 16:53:19 +0800

    castings call logs

---
+++ src/models/cd_log.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var _ = require('lodash');
+
+function CDLogs(data) {
+	_.extend(this, data);
+}
+
+CDLogs.relationship = [
+	'data:cd_logs'
+];
+module.exports = CDLogs;
---
+++ src/models/cd_logs.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var _ = require('lodash'),
+	cdLogs = {};
+
+
+cdLogs.create = function(array) {
+	var modelify = require('src/services/model.js');
+
+	return _.map(array || [], function(item) {
+		return modelify('cd_log', item);
+	});
+};
+
+module.exports = cdLogs;
