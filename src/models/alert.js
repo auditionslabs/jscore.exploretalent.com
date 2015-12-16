932b7785b9b76714bdca31910ee1baa737dd62ea
Josh Dabuet <joshd.utech@gmail.com>
2015-12-16 17:40:59 +0800

    Added staff alert resource

---
+++ src/models/alert.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var _ = require('lodash');
+
+function Alert(data) {
+	_.extend(this, data || {});
+}
+
+Alert.relationship = [
+	'data:alerts'
+];
+
+module.exports = Alert;
---
+++ src/models/alerts.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var _ = require('lodash'),
+	alerts = {},
+	Alert = require('src/models/alert.js');
+
+alerts.relationship = Alert.relationship;
+
+alerts.create = function(array) {
+	var modelify = require('src/services/model.js');
+
+	return _.map(array || [], function(item) {
+		return modelify('alert', item);
+	});
+};
+
+module.exports = alerts;
