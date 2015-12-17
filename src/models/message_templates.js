406bb11f2892a3a1055636f18cd6747d7fa0c7ba
Sigmund Burgos <sigmund.burgos@gmail.com>
2015-12-17 15:49:31 +0800

    Wrong speling.

--- src/models/message_templates.js
+++ src/models/message_templates.js
@@ -7,7 +7,7 @@ message_templates.create = function(array) {
 	var modelify = require('src/services/model.js');
 
 	return _.map(array || [], function(item) {
-		return modelify('message_templates', item);
+		return modelify('message_template', item);
 	});
 };
 
