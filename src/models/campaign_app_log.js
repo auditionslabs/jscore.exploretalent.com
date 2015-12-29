ee06b30cd42338a2452ee82b6fcf5da140a2ac22
Sigmund Burgos <sigmund.burgos@gmail.com>
2015-12-29 13:03:43 +0800

    Added campaign app logs model and resource.

---
+++ src/models/campaign_app_log.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var _ = require('lodash');
+
+function CampaignAppLog(data) {
+	_.extend(this, data || {});
+}
+
+CampaignAppLog.relationship = [
+	'data:campaign_app_logs'
+];
+
+module.exports = CampaignAppLog;
