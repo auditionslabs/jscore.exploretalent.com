bf6502857666d17344ee46e52d6a78a608d77208
Josh Dabuet <joshd.utech@gmail.com>
2016-01-15 19:02:42 +0800

    Payments Model and Resource

---
+++ src/models/payment.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var _ = require('lodash');
+
+function Payments(data) {
+	_.extend(this, data || {});
+}
+
+Payments.relationship = [
+	'data:payments'
+];
+
+module.exports = Payments;
---
+++ src/models/payments.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var _ = require('lodash'),
+	payments = {},
+	Payments = require('src/models/payment.js');
+
+payments.relationship = Payments.relationship;
+
+payments.create = function(array) {
+	var modelify = require('src/services/model.js');
+
+	return _.map(array || [], function(item) {
+		return modelify('payment', item);
+	});
+};
+
+module.exports = payments;
