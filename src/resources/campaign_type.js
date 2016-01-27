215026c049c82ac3eceff57f21bc3f033cead9f8
Sigmund Burgos <sigmund.burgos@gmail.com>
2016-01-27 13:44:12 +0800

    Added more campaign types.

--- src/resources/campaign_type.js
+++ src/resources/campaign_type.js
@@ -1,6 +1,8 @@
 'use strict';
 
 module.exports = {
-	CRM : 1,
-	CD_INVITE : 2
+	NORMAL      : 1, // normal campaigns
+	CD_INVITE   : 2, // cd invites
+	TRM         : 3, // triggered / transactional
+	CAST_MATCH  : 4 // cast match
 }
