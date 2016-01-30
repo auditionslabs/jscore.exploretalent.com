c0f1d7f8fb1315f71c77ff750e159772b9f33171
Josh Dabuet <joshd.utech@gmail.com>
2016-01-30 16:41:00 +0800

    Date service function formatDateTime (if hour is 00, display 12)

--- src/services/date.js
+++ src/services/date.js
@@ -72,7 +72,7 @@ function formatDateTime(value) {
 		return date.getUTCFullYear() + '-' +
 			_.padLeft(date.getUTCMonth() + 1, 2, 0) + '-' +
 			_.padLeft(date.getUTCDate(), 2, 0) + ' ' +
-			_.padLeft((date.getUTCHours() > 12 ? date.getUTCHours() - 12 : date.getUTCHours()), 2, 0) + ':' +
+			_.padLeft((date.getUTCHours() > 12 ? date.getUTCHours() - 12 : (date.getUTCHours() == '00') ? '12' : date.getUTCHours()), 2, 0) + ':' +
 			_.padLeft(date.getUTCMinutes(), 2, 0) + ' ' +
 			(date.getUTCHours() < 12 ? 'AM' : 'PM');
 	}
