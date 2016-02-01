5a7558a73a13073c456ad03539f92e8ea39d36ca
Josh Dabuet <joshd.utech@gmail.com>
2016-02-01 20:30:12 +0800

    TRM Feedback (Admin Side) model and resource

---
+++ src/models/trm_feedbacks.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var _ = require('lodash'),
+	trm_feedbacks = {};
+
+trm_feedbacks.create = function(array) {
+	var modelify = require('src/services/model.js');
+
+	return _.map(array || [], function(item) {
+		return modelify('trm_feedback', item);
+	});
+};
+
+module.exports = trm_feedbacks;
