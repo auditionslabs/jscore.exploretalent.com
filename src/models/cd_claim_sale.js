c8b8a840d7fbd39354cf04e992475a21c2901296
Josh Dabuet <joshd.utech@gmail.com>
2016-02-23 14:00:54 +0800

    CD Claim Sales Resource and Model

---
+++ src/models/cd_claim_sale.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var _ = require('lodash');
+
+function CdClaimSale(data) {
+	_.extend(this, data || {});
+}
+
+CdClaimSale.relationship = [
+	'data:cd_claim_sales'
+];
+
+module.exports = CdClaimSale;
