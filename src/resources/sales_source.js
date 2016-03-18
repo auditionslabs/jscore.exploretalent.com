9ae8df61fb775ffc8d3806e92db02576ce1e642f
Josh Dabuet <joshd.utech@gmail.com>
2016-03-18 15:30:28 +0800

    Fixed route issue Sales Source resource

--- src/resources/sales_source.js
+++ src/resources/sales_source.js
@@ -3,6 +3,6 @@
 var api = require('src/config/api.js'),
 	Resource = require('src/services/resource.js');
 
-module.exports = new Resource(api.base + api.type + '/sales_sources/:sourceId', {
+module.exports = new Resource(api.base + api.type + '/sale_sources/:sourceId', {
 	model: 'sales_source'
 });
