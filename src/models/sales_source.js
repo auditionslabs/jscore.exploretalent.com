275ad2c9d2dd379e9f8fcd6f487cbf6efcafce41
Josh Dabuet <joshd.utech@gmail.com>
2016-03-18 15:07:13 +0800

    Sales Sources Resource and Model

---
+++ src/models/sales_source.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var _ = require('lodash');
+
+function SalesSource(data) {
+	_.extend(this, data || {});
+}
+
+SalesSource.relationship = [
+	'data:sales_sources'
+];
+
+module.exports = SalesSource;
---
+++ src/models/sales_sources.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var _ = require('lodash'),
+	sales_sources = {};
+
+sales_sources.create = function(array) {
+	var modelify = require('src/services/model.js');
+
+	return _.map(array || [], function(item) {
+		return modelify('sales_source', item);
+	});
+};
+
+module.exports = sales_sources;
