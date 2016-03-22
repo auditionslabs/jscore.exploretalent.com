773243de69b912043890b9cc9047d871bb4989b5
Daniel Cabale <dcabale.dc@gmail.com>
2016-03-22 10:43:09 -0400

    added dynamic type to campaign_type.js

--- src/resources/campaign_type.js
+++ src/resources/campaign_type.js
@@ -4,6 +4,6 @@ module.exports = {
 	NORMAL      : 1, // normal campaigns
 	CD_INVITE   : 2, // cd invites
 	TRM         : 3, // triggered / transactional
-	CAST_MATCH  : 4 // cast match
+	CAST_MATCH  : 4, // cast match
 	DYN			: 5 // dynamic campaign
 }
