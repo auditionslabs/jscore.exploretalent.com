281f7b53f5560cd6b78b86ef3c693abdcdd7524e
Josh Dabuet <joshd.utech@gmail.com>
2016-03-29 16:05:33 +0800

    Vicidial Users Resource and Model

---
+++ src/models/vicidial_user.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var _ = require('lodash');
+
+function VicidialUsers(data) {
+	_.extend(this, data || {});
+}
+
+VicidialUsers.relationship = [
+	'data:vicidial_users'
+];
+
+module.exports = VicidialUsers;
---
+++ src/models/vicidial_users.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var _ = require('lodash'),
+	vicidial_userss = {};
+
+vicidial_userss.create = function(array) {
+	var modelify = require('src/services/model.js');
+
+	return _.map(array || [], function(item) {
+		return modelify('vicidial_user', item);
+	});
+};
+
+module.exports = vicidial_userss;
