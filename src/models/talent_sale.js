a327f2541f1f97b8769730db7819696cdb85a5e9
Josh Dabuet <joshd.utech@gmail.com>
2016-04-04 23:08:56 +0800

    Sales New API for orders

---
+++ src/models/talent_sale.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var _ = require('lodash');
+
+function TalentSales(data) {
+	_.extend(this, data || {});
+}
+
+TalentSales.relationship = [
+	'data:talent_sales'
+];
+
+module.exports = TalentSales;
---
+++ src/models/talent_sales.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var _ = require('lodash'),
+	talent_saless = {};
+
+talent_saless.create = function(array) {
+	var modelify = require('src/services/model.js');
+
+	return _.map(array || [], function(item) {
+		return modelify('talent_sale', item);
+	});
+};
+
+module.exports = talent_saless;
