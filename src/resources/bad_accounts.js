dc39ed91838940aa7cee2c71dfdd10b7c651b53d
Josh Dabuet <joshd.utech@gmail.com>
2016-04-04 18:12:36 +0800

    Bad Accounts resource primary key changed to talentnum

--- src/resources/bad_accounts.js
+++ src/resources/bad_accounts.js
@@ -3,6 +3,6 @@
 var api = require('src/config/api.js'),
 	Resource = require('src/services/resource.js');
 
-module.exports = new Resource(api.base + api.type + '/bad_accounts/:ccNum', {
+module.exports = new Resource(api.base + api.type + '/bad_accounts/:talentnum', {
 	model: 'bad_account'
 });
