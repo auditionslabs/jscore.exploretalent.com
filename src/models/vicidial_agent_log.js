7542c95fec3c7e7e4b74cb6650885f7ad59791a5
Josh Dabuet <joshd.utech@gmail.com>
2016-04-20 16:46:04 +0800

    Vicidial Agent Log Resource and Model

---
+++ src/models/vicidial_agent_log.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var _ = require('lodash');
+
+function VicidialAgentLogs(data) {
+	_.extend(this, data || {});
+}
+
+VicidialAgentLogs.relationship = [
+	'data:vicidial_agent_logs'
+];
+
+module.exports = VicidialAgentLogs;
---
+++ src/models/vicidial_agent_logs.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var _ = require('lodash'),
+	vicidial_agent_logss = {};
+
+vicidial_agent_logss.create = function(array) {
+	var modelify = require('src/services/model.js');
+
+	return _.map(array || [], function(item) {
+		return modelify('vicidial_agent_log', item);
+	});
+};
+
+module.exports = vicidial_agent_logss;
