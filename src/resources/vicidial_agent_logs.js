585e344a830b01f61e76becf6c8829d1364b7c92
Josh Dabuet <joshd.utech@gmail.com>
2016-04-20 17:03:22 +0800

    Vicidial Agent Log resource fix

--- src/resources/vicidial_agent_logs.js
+++ src/resources/vicidial_agent_logs.js
@@ -3,6 +3,6 @@
 var api = require('src/config/api.js'),
 	Resource = require('src/services/resource.js');
 
-module.exports = new Resource(api.base + api.type + '/agent_logs/:agentLogId', {
+module.exports = new Resource(api.base + api.type + '/vicidial/agent_logs/:agentLogId', {
 	model: 'vicidial_agent_log'
 });
