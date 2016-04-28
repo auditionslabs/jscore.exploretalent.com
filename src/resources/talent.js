2afcd1b5e16f9efa038af65bef9a1d1f01ed7240
Sigmund Burgos <sigmund.utech@gmail.com>
2016-04-28 17:22:29 +0800

    Added checking for like it list.

--- src/resources/talent.js
+++ src/resources/talent.js
@@ -7,68 +7,98 @@ var resource = new Resource(api.base + api.type + '/talentci/:talentId', {
 	model: 'bam_talentci'
 });
 
-resource.search = function(data) {
+resource.search = function(data, options) {
 	var deferred = $.Deferred();
-	var talents, talentnums;
+	var talents, talentnums, users;
 
+	// search talent from search/talents
 	self.core.resource.search_talent.get(data)
 		.then(function(res) {
+			// assign to a variable
 			talents = res;
-			if (talents.total) {
-				talentnums = _.map(talents.data, function(talent) {
-					return talent.talentnum;
-				});
 
-				talentnums.push(0);
+			// get all talentnums
+			talentnums = _.map(talents.data, function(talent) {
+				return talent.talentnum;
+			});
 
-				var data2 = {
-					query : [
-						[ 'whereIn', 'talentci.talentnum', talentnums ],
-						[ 'with', 'bam_talent_media2' ],
-						[ 'with', 'user' ]
-					]
-				};
-				return self.core.resource.talent.get(data2);
-			}
-			else {
-				return $.when({ data : [] });
-			}
+			// add 0 so we dont have an empty array
+			talentnums.push(0);
+
+			// get all user.id
+			users = _.map(talents.data, function(talent) {
+				return talent.user.id;
+			});
+
+			// add 0 so we dont have an empty array
+			users.push(0);
+
+			// get user and bam_talent_media2 objects using talentci resource
+			data = {
+				query : [
+					[ 'whereIn', 'talentci.talentnum', talentnums ],
+					[ 'with', 'bam_talent_media2' ],
+					[ 'with', 'user' ]
+				]
+			};
+
+			return self.core.resource.talent.get(data);
 		})
 		.then(function (res) {
+			// loop through talents and find talent_media2 object
 			_.each(talents.data, function(talent) {
-				var talentci = _.find(res.data, function(tm) {
-					return talent.talentnum == tm.talentnum;
+				// find talentci object
+				var talentci = _.find(res.data, function(t) {
+					return talent.talentnum == t.talentnum;
 				});
 
+				// if we have talentci object, assign its talent_media2 and user object to the talent
 				if (talentci) {
 					talent.bam_talent_media2 = talentci.bam_talent_media2;
 					talent.user = talentci.user;
 				}
 			});
 
-			if (talents.total) {
-				// get favorite talents
-				var data2 = {
-					q : [
-						[ 'whereIn', 'bam_talentnum', talentnums ]
-					]
-				};
+			// get favorite talents
+			data = {
+				q : [
+					[ 'whereIn', 'bam_talentnum', talentnums ]
+				]
+			};
 
-				return self.core.resource.favorite_talent.get(data2);
+			return self.core.resource.favorite_talent.get(data);
+		})
+		.then(function(res) {
+			//assign favorite talents to talent
+			_.each(talents.data, function(talent) {
+				// find favorite object and assign it
+				talent.favorite = _.find(res.data, function(favorite) {
+					return talent.talentnum == favorite.bam_talentnum;
+				});
+			});
+
+			// if bam_role_id is given, search if talent is on likeitlist for it
+			if (options.bam_role_id) {
+				data = {
+					q : [
+						[ 'whereIn', 'invitee_id', users ],
+						[ 'where', 'bam_role_id', '=', options.bam_role_id ],
+						[ 'where', 'rating', '<>', 0 ]
+					]
+				}
+
+				return self.core.resource.schedule.get(data);
 			}
 			else {
 				return $.when({ data : [] });
 			}
 		})
 		.then(function(res) {
-			if (talents.total) {
-				//assign favorite talents to talent
-				_.each(talents.data, function(talent) {
-					talent.favorite = _.find(res.data, function(favorite) {
-						return talent.talentnum == favorite.bam_talentnum;
-					});
+			_.each(talents.data, function(talent) {
+				talent.schedule = _.find(res.data, function(schedule) {
+					return talent.user.id == schedule.invitee_id;
 				});
-			}
+			});
 
 			deferred.resolve(talents);
 		});
