9a314b2c7253e99f11f4c37f6a4a63ef4b1c4d16
Josh Dabuet <joshd.utech@gmail.com>
2016-06-23 14:19:03 +0800

    App_Xorigins Model & Resource

---
+++ src/models/app_xorigin.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var _ = require('lodash');
+
+function AppXorigins(data) {
+	_.extend(this, data || {});
+}
+
+AppXorigins.relationship = [
+	'data:app_xorigins'
+];
+
+module.exports = AppXorigins;
---
+++ src/models/app_xorigins.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var _ = require('lodash'),
+	app_xorigins = {};
+
+app_xorigins.create = function(array) {
+	var modelify = require('src/services/model.js');
+
+	return _.map(array || [], function(item) {
+		return modelify('app_xorigin', item);
+	});
+};
+
+module.exports = app_xorigins;
