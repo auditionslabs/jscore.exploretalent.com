e581f30a8ab3c3a30c9ceea36af85abf63331511
Jef <jef.utech@gmail.com>
2016-12-16 17:55:53 +0800

    projectapp

---
+++ src/models/project_app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var _ = require('lodash');
+
+function ProjectApp(data) {
+	_.extend(this, data);
+}
+
+ProjectApp.relationship = [
+	'data:project_apps'
+];
+module.exports = ProjectApp;
---
+++ src/models/project_apps.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var _ = require('lodash'),
+	project_apps = {};
+
+project_apps.create = function(array) {
+	var modelify = require('src/services/model.js');
+
+	return _.map(array || [], function(item) {
+		return modelify('project_app', item);
+	});
+};
+
+module.exports = project_apps;
