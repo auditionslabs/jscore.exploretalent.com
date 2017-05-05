e03b9be94a13545da0c2576d5c2a1f2529b898fe
alicia <alicia.utech@gmail.com>
2017-05-05 23:47:50 +0800

    Magic

---
+++ src/models/casting_app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var _ = require('lodash');
+
+function CastingApp(data) {
+	_.extend(this, data);
+}
+
+module.exports = CastingApp;
---
+++ src/models/casting_apps.js
@@ -0,0 +1,15 @@
+var _ = require('lodash'),
+	casting_apps = {},
+	CastingApp = require('src/models/casting_app.js');
+
+casting_apps.relationship = CastingApp.relationship;
+
+casting_apps.create = function(array) {
+	var modelify = require('src/services/model.js');
+
+	return _.map(array || [], function(item) {
+		return modelify('casting_app', item);
+	});
+};
+
+module.exports = casting_apps;
