f08e26081b297e0e0d6da4b11d5a05c8f94bced9
John Baguio <john.baguio.utech@gmail.com>
2017-07-12 18:17:36 +0800

    Fixed typo error

--- src/resources/cd_user_campaign_subscription.js
+++ src/resources/cd_user_campaign_subscription.js
@@ -4,5 +4,5 @@ var api = require('src/config/api.js'),
 	Resource = require('src/services/resource.js');
 
 module.exports = new Resource(api.base + api.type + '/user_campaign_subscriptions/:id', {
-	model: 'cd_user_campaign_subsription'
+	model: 'cd_user_campaign_subscription'
 });
