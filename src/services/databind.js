65ae95ca3bc37d3d25a1e624167c082417290e8a
Barry Deeney <mxaddict@codedmaster.com>
2018-02-13 19:30:30 +0800

    Ooops

--- src/services/databind.js
+++ src/services/databind.js
@@ -159,7 +159,7 @@ function databind (element, data, append) {
         $(element).attr('value', value.key)
         $(element).text(value.value)
         if (value.selected && value.selected.length > 0) {
-          $(element).parent().val(value.value)
+          $(element).parent().val(value.key)
         }
         break
       case 'class':
