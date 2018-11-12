cde22ca0a575be04a6d6add16b7af48550e48df6
Alicia Escarda <escardaaliciamae@gmail.com>
2018-11-12 15:29:24 +0800

    modify search projects jscore

--- src/resources/search_project.js
+++ src/resources/search_project.js
@@ -3,6 +3,6 @@
 let api = require('src/config/api.js'),
   Resource = require('src/services/resource.js')
 
-module.exports = new Resource(api.base + api.type + '/search/projects/:id', {
+module.exports = new Resource(api.base + '/search/projects/:id', {
   model: 'bam_casting'
 })
