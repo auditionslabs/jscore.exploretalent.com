f19794da5940b2bb6ba4db467d81583cadcfc6a2
Francis Alec Yap <francisj.yap@gmail.com>
2018-12-21 14:53:51 +0800

    link campaig typo

--- src/resources/link_campaign.js
+++ src/resources/link_campaign.js
@@ -3,6 +3,6 @@
 let api = require('src/config/api.js'),
   Resource = require('src/services/resource.js')
 
-module.exports = new Resource(api.base + api.type + '/v1/admin/link_campaigns/:id', {
+module.exports = new Resource(api.base + api.type + '/link_campaigns/:id', {
   model: 'link_campaign'
 })
