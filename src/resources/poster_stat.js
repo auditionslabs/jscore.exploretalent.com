dd4347388edf876ccfab257a85baa533c106d07f
Alicia Escarda <escardaaliciamae@gmail.com>
2019-01-10 17:12:36 +0800

    YAWA

--- src/resources/poster_stat.js
+++ src/resources/poster_stat.js
@@ -3,6 +3,6 @@
 let api = require('src/config/api.js'),
   Resource = require('src/services/resource.js')
 
-module.exports = new Resource(api.base + api.type + '/poster_stats:id', {
+module.exports = new Resource(api.base + api.type + '/poster_stats/:id', {
   model: 'poster_stats'
 })
