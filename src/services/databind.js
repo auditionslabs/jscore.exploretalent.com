6310cd650c80ac4c6796e6384aff13b671361c87
Barry Deeney <mxaddict@codedmaster.com>
2019-02-16 00:14:08 +0800

    Updates

--- src/services/databind.js
+++ src/services/databind.js
@@ -79,11 +79,11 @@ function databind (element, data, append) {
         attr = 'multiselect'
       } else if ($(element).is('[data-select]')) {
         attr = 'select'
-      } else if ($(element).is('input') || $(element).is('select')) {
+      } else if ($(element).is('input') || $(element).is('select') || $(element).is('textarea')) {
         attr = 'val'
       } else if ($(element).is('a')) {
         attr = 'href'
-      } else if ($(element).is('p') || $(element).is('span') || $(element).is('b') || $(element).is('td') || $(element).is('h2') || $(element).is('h4') || $(element).is('textarea') || $(element).is('div') || $(element).is('strong')) {
+      } else if ($(element).is('p') || $(element).is('span') || $(element).is('b') || $(element).is('td') || $(element).is('h2') || $(element).is('h4') || $(element).is('div') || $(element).is('strong')) {
         attr = 'text'
       } else if ($(element).is('img') || $(element).is('iframe')) {
         attr = 'src'
