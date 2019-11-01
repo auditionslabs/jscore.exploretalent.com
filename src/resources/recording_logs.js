564179b4bb088f14c89ef87efc87ebaf3b9b4f33
Francis Alec Yap <francisj.yap@gmail.com>
2019-11-01 17:58:32 +0800

    added recording logs model

--- src/resources/recording_logs.js
+++ src/resources/recording_logs.js
@@ -3,6 +3,6 @@
 let api = require('src/config/api.js'),
   Resource = require('src/services/resource.js')
 
-module.exports = new Resource(api.base + api.type + '/admin/asterisk/recording_logs/:uniqueid', {
+module.exports = new Resource(api.base + api.type + '/asterisk/recording_logs/:uniqueid', {
   model: 'recording_log'
 })
