cbc0ebecd3dc9371621871698b0211ac7fa93764
Francis Alec Yap <francisj.yap@gmail.com>
2020-04-23 14:51:19 +0800

    Added primary key param in vicidial recs

--- src/resources/vicidial_recs.js
+++ src/resources/vicidial_recs.js
@@ -3,6 +3,6 @@
 let api = require('src/config/api.js'),
   Resource = require('src/services/resource.js')
 
-module.exports = new Resource(api.base + api.type + '/et/vicidial_recs', {
+module.exports = new Resource(api.base + api.type + '/et/vicidial_recs/:vd_id', {
   model: 'vicidial_recs'
 })
