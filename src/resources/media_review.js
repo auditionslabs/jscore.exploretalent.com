507959e527333287666d736151cae3fa05b5f80d
Ronak <ronak@twopeople.company>
2022-05-30 14:17:38 +0530

    added media review resourse

---
+++ src/resources/media_review.js
@@ -0,0 +1,8 @@
+'use strict'
+
+let api = require('src/config/api.js'),
+  Resource = require('src/services/resource.js')
+
+module.exports = new Resource(api.base + api.type + '/pictures_for_approval', {
+  model: 'campaign_app'
+})
